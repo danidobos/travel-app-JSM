15be229cbe334d2ea334c9861344576dd7dad011
danidobos <danidobos97@gmail.com>
2024-04-27 13:04:09 +0200

    Create CTA section

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,20 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 type ButtonProps = {
-  type: "button" | "submit";
+  type: 'button' | 'submit';
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 cursor-pointer whitespace-nowrap">
+        {title}
+      </label>
     </button>
   );
 };
